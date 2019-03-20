ccd92f7d28365bbb92dc2963992b4cb8b83bb3b1
OscarGB <oscar.gomez.borz@gmail.com>
2019-03-20 19:59:12 +0100

    ej3, falta cliente local

--- P2/limpiar-y-desplegar.sh
+++ P2/limpiar-y-desplegar.sh
@@ -5,10 +5,6 @@ for i in P1-base P1-ws P1-ejb-servidor-remoto P1-ejb-cliente-remoto; do
 	cd -
 done
 
-cd P1-base
-ant delete-db
-cd -
-
 for i in P1-base P1-ws P1-ejb-servidor-remoto P1-ejb-cliente-remoto; do
 	cd $i
 	ant limpiar-todo todo
